005f15fd30c03efa7e832276233a750726d07620
sacylkowski <sacylkowski@gmail.com>
2022-04-19 16:08:28 -0500

    added sql queries to server file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +7,53 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // your SQL username,
+        user: "root",
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello World!"
-    });
+        // your SQL password
+        password: "Bananabread27",
+
+        database: "election"
+    },
+    console.log("Connected to the election database.")
+);
+
+// the db object is using the query() method.  This method runs the SQL query and executes the callback with resulting rows that match
+// err is the error response and rows is the database query response
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    // console.log(rows);
 });
+
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    // if (err) {
+    //     console.log(err);
+    // }
+    // console.log(row);
+});
+
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    // if (err) {
+    //     console.log(err);
+    // }
+    // console.log(result);
+});
+
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+VALUES (?, ?, ?, ?)`;
+
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
