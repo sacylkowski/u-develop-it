c7c90c649c69952dc5f9d35ec1e5f1d923f13952
sacylkowski <sacylkowski@gmail.com>
2022-04-19 16:48:52 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const express = require("express");
+const inputCheck = require("./utils/inputCheck");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -24,42 +25,93 @@ const db = mysql.createConnection(
 
 // the db object is using the query() method.  This method runs the SQL query and executes the callback with resulting rows that match
 // err is the error response and rows is the database query response
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    // console.log(rows);
+// the query is wrapped in an Express.js route
+app.get("/api/candidates", (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            // sending a status code of 500 and placing the error message wit hin a JSON object
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
 });
 
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    // if (err) {
-    //     console.log(err);
-    // }
-    // console.log(row);
+app.get("/api/candidate/:id", (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, rows) => {
+        if (err) {
+            // sending a status code of 400 and placing the error message wit hin a JSON object
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
 });
 
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-    // if (err) {
-    //     console.log(err);
-    // }
-    // console.log(result);
+app.delete("/api/candidate/:id", (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, rows) => {
+        if (err) {
+            // sending a status code of 400 and placing the error message wit hin a JSON object
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: "Candidate not found"
+            });
+        } else {
+            res.json({
+                message: "deleted",
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
 });
 
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-VALUES (?, ?, ?, ?)`;
-
-const params = [1, "Ronald", "Firbank", 1];
-
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
+// using the object req.body to populate the candidate's data
+// using object destructing to pull the body property out of the request object 
+app.post("/api/candidate", ({ body }, res) => {
+    const errors = inputCheck(body, "first_name", "last_name", "industry_connected");
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
     }
-    console.log(result);
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+            VALUES (?, ?, ?)`;
+
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: body
+        });
+    });
 });
 
 
-// default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-});
+    // default response for any other request (Not Found)
+    app.use((req, res) => {
+        res.status(404).end();
+    });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
